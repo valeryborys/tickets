16717cb336aa8d7eaae21f529dbe759b19acf9d1
Valery Borys <valeryborys@gmail.com>
2021-02-04 00:28:04 +0300

    fix

--- src/main/java/com/anylogic/testproject/Main.java
+++ src/main/java/com/anylogic/testproject/Main.java
@@ -16,24 +16,22 @@ import com.anylogic.testproject.parser.ParserImpl;
 
 public class Main {
 	private static Logger logger = LogManager.getLogger(Main.class);
-private static final String FILE_NAME = "tickets.json";
+	private static final String FILE_NAME = "tickets.json";
+
 	public static void main(String[] args) {
 		Parser parser = ParserImpl.PARSER;
 		Handler handler = HandlerImpl.HANDLER;
 		try {
 			String json = parser.getJsonAsString(FILE_NAME);
 			List<Ticket> tickets = parser.parseTickets(json);
-			if (tickets != null) {
-				for (Ticket ticket : tickets) {
-					System.out.println(ticket);
-				}
-				System.out.printf("Average flight time from Vladivostok to Tel Aviv is %s\n",
-						handler.getAverageTime(tickets));
-				Double percentil = handler.getPercentil(tickets, 0.9);
-				System.out.printf("90-s percentil of of flight time is %.1f\n", percentil);
-			} else {
-				System.out.println("Not wellformed JSON file! Please, check JSON format and try again.");
+			for (Ticket ticket : tickets) {
+				System.out.println(ticket);
 			}
+			System.out.printf("Average flight time from Vladivostok to Tel Aviv is %s\n",
+					handler.getAverageTime(tickets));
+			Double percentil = handler.getPercentil(tickets, 0.9);
+			System.out.printf("90-s percentil of of flight time is %.1f\n", percentil);
+
 		} catch (JsonTicketException e) {
 			logger.error("Not wellformed JSON file! Please, check JSON format and try again.");
 		}
